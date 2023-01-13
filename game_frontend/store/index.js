2d5bf7cbe8136142a73c091fdaffd4c834b55f38
leoyongyuan <1359195795@qq.com>
2023-01-13 18:37:26 +0800

    chore: 清除store

--- game_frontend/store/index.js
+++ game_frontend/store/index.js
@@ -5,34 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    $PlayGround: {},
-    $canvas: {},
-    Game_OBJECTS: [],
+    
   },
 
   getters: {
   },
   
   mutations: {
-    getPlayGround(state, { width, height }) {
-      state.$PlayGround.width = width
-      state.$PlayGround.height = height
-    },
-    getCanvas(state, value) {
-      state.$canvas = value
-    },
-    addGameObject(state, value) {
-      state.Game_OBJECTS.push(value)
-    },
-    destroyGameObject(state,value) {
-      // 找到数组中对应的对象，删掉
-      for (let i = 0; i < state.Game_OBJECTS.length; i ++ ) {
-        if (state.Game_OBJECTS[i] === value) {
-          state.Game_OBJECTS.splice(i,1);
-          break;
-        }
-      }
-    }
+
   },
 
   actions: {
