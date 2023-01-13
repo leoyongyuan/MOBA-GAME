9c17d6729ecc0d58fd44dd7a7a910e0cefc7db3e
leoyongyuan <1359195795@qq.com>
2023-01-13 18:35:45 +0800

    chore: 删掉一些无用的注释

--- game_backend/urls.py
+++ game_backend/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-# from game_backend.views import index
 from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html")),
