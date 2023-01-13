d718e568e704d4d5d9bdcc5310063cbb82af6dee
leoyongyuan <1359195795@qq.com>
2023-01-13 15:51:37 +0800

    refactor: 重构Player中的方法函数

--- game_frontend/src/components/Game_PlayGround/Player/index.js
+++ game_frontend/src/components/Game_PlayGround/Player/index.js
@@ -19,36 +19,70 @@ export class Player extends GameObject{
   }
 
   start() {
-
+    if (this.is_me) {
+      this.listeningEvents()
+    }
   }
 
-  update() {
-    // if (this.move_length < this.eps) {
-    //   this.move_length = 0;
-    //   this.vx = this.vy = 0;
-    // } else {
-    //   let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);
-    //   this.x += this.vx * moved;
-    //   this.y += this.vy * moved;
-    //   this.move_length -= moved;
-    // }
+  // 角色的所有监听函数
+  listeningEvents() {
+    this.playground.gameMap.$canvas.on("contextmenu", function() {
+      return false;
+    });
 
-    this.render()
+    this.playground.gameMap.$canvas.on('mousedown',e => {
+      if (e.which === 3) {         // 右键
+        this.moveTO(e.clientX,e.clientY)
+      } else if (e.which === 1) {   // 左键
+        if (this.curSkill === 'fireBall') {
+          // this.shootFireBall(e.clientX,e.clientY)
+        }
+      }
+      this.curSkill = null
+    });
+
+    window.addEventListener('keydown',e => {
+      if (e.which === 81) {  // q 
+        this.curSkill = 'fireBall';
+        return;
+      }
+    })
   }
 
-  get_dist(x1, y1, x2, y2) {
+  // 求两点之前的欧几里得距离
+  getDist(x1, y1, x2, y2) {
     let dx = x1 - x2;
     let dy = y1 - y2;
     return Math.sqrt(dx * dx + dy * dy);
   }
 
-  move_to(tx, ty) {
-    this.move_length = this.get_dist(this.x, this.y, tx, ty);
+  // 移动角色
+  moveTO(tx, ty) {
+    this.move_length = this.getDist(this.x, this.y, tx, ty);
+    // 获取移动方向在坐标系下的角度
     let angle = Math.atan2(ty - this.y, tx - this.x);
-    this.vx = Math.cos(angle);
+    this.vx = Math.cos(angle);  
     this.vy = Math.sin(angle);
   }
 
+
+  update() {
+    // 查看是否走到了终点
+    if (this.move_length < this.eps) {
+      this.move_length = 0;
+      this.vx = this.vy = 0;
+    } else {
+      // 算出每一时间戳的距离
+      let moved = Math.min(this.move_length, this.speed * this.timeDelta / 1000);
+      this.x += this.vx * moved;
+      this.y += this.vy * moved;
+      this.move_length -= moved;
+    }
+
+    this.render()
+  }
+
+  // 渲染玩家
   render() {
     this.ctx.beginPath(); //开始绘制
     this.ctx.arc(this.x, this.y, this.radius,0,2 * Math.PI,false); //arc 的意思是“弧”
